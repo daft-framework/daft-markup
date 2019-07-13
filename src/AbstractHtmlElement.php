4a226139dbcc1b4b40a9116a62be2d12170fb12b
SignpostMarv <me@signpostmarv.name>
2019-07-13 09:15:25 +0100

    satisfying static analysis

--- src/AbstractHtmlElement.php
+++ src/AbstractHtmlElement.php
@@ -56,6 +56,9 @@ abstract class AbstractHtmlElement
     */
     public function ToMarkupArray(array $content = null) : array
     {
+        /**
+        * @var array{!element:T1, !attributes:T2}
+        */
         $out = [
             '!element' => static::MarkupElementName(),
             '!attributes' => $this->MarkupAttributes(),
